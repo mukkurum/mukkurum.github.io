9eb881991b4735b6b6a3f8c529a9df0b395107bd
mukkurum <belucasion@gmail.com>
2018-12-17 21:01:19 +0900

    project

---
+++ assets/js/killtheking.js
@@ -0,0 +1,62 @@
+var pageNum = 0
+var maxPageNum = 5
+var starData = [
+    ["킬더킹(0-19)", 9.81, 9.87, 9.88, 9.87, 9.74, 9.78, 9.84, 9.84, 9.82, 9.84, 9.84, 9.81, 9.80, 9.81, 9.84, 9.83, 9.82, 9.82, 9.81, 9.80],
+    ["킬더킹(20-39)", 9.80, 9.78, 9.87, 9.78, 9.71, 9.67, 9.60, 9.20, 7.33, 8.39, 9.08, 9.18, 9.23, 9.41, 9.54, 9.62, 9.73, 9.69, 9.84, 9.77],
+    ["킬더킹(40-59)", 9.79, 9.81, 9.88, 9.86, 9.88, 9.86, 9.87, 9.84, 9.83, 9.83, 9.82, 9.81, 9.83, 9.86, 9.85, 9.82, 9.53, 8.97, 8.86, 9.74],
+    ["킬더킹(60-79)", 9.86, 9.87, 9.85, 9.85, 9.69, 9.42, 8.44, 8.42, 7.33, 6.53, 8.44, 8.90, 9.10, 9.23, 9.49, 9.36, 9.73, 9.70, 9.78, 9.78],
+    ["킬더킹(80-99)", 9.88, 9.74, 9.84, 9.87, 9.85, 9.90, 9.93, 9.91, 9.92, 9.92, 9.93, 9.93, 9.94, 9.91, 9.89, 9.93, 9.93, 9.92, 9.92, 9.92]
+]
+
+window.addEventListener("load", function(event) {
+    var chart = bb.generate({
+        data: {
+            columns: [starData[pageNum]]
+        },
+        axis: {
+            y: {
+              max: 10,
+              min: 1
+            }
+        },
+        bindto: "#Chart"
+    }) 
+});
+
+var MovePage = new Vue({
+    el: '#section',
+    methods: {
+        next: function () {
+            if (pageNum < maxPageNum) pageNum++
+            else alert("마지막 페이지입니다.")
+            var chart = bb.generate({
+                data: {
+                    columns: [starData[pageNum]]
+                },
+                axis: {
+                    y: {
+                      max: 10,
+                      min: 1
+                    }
+                },
+                bindto: "#Chart"
+            });
+        },
+        prev: function () {
+            if (pageNum > 0) pageNum--
+            else alert("처음 페이지입니다.")
+            var chart = bb.generate({
+                data: {
+                    columns: [starData[pageNum]]
+                },
+                axis: {
+                    y: {
+                      max: 10,
+                      min: 1
+                    }
+                  },
+                bindto: "#Chart"
+            });
+        }
+    }
+})
---
+++ assets/js/zeroalliance.js
@@ -0,0 +1,76 @@
+var pageNum = 0
+var maxPageNum = 4
+var starData = [
+    ["빵점동맹(0-19)", 9.76, 9.81, 9.85, 9.86, 9.84, 9.87, 9.84, 9.76, 9.49, 9.55, 9.52, 9.07, 9.74, 9.62, 9.63, 9.32, 9.52, 9.58, 8.72, 8.97],
+    ["빵점동맹(20-39)", 9.46, 9.49, 9.44, 9.32, 9.04, 9.47, 9.48, 9.82, 9.84, 9.83, 9.86, 9.85, 9.90, 9.84, 9.69, 9.62, 9.72, 9.72, 9.82, 9.58],
+    ["빵점동맹(40-59)", 9.13, 9.19, 9.71, 8.72, 7.20, 9.65, 9.82, 9.75, 9.64, 9.82, 9.88, 9.80, 9.84, 9.71, 9.71, 9.83, 9.74, 9.36, 9.43, 9.61],
+    ["빵점동맹(60-74)", 9.69, 9.55, 8.79, 8.67, 9.89, 9.94, 9.95, 9.95, 9.94, 9.90, 9.84, 9.81, 9.94, 9.87],
+]
+
+window.addEventListener("load", function(event) {
+    var chart = bb.generate({
+        data: {
+            columns: [starData[pageNum]]
+        },
+        axis: {
+            y: {
+              max: 10,
+              min: 1
+            }
+        },
+        color: {
+            pattern: [
+                "#ffe874"
+            ]
+        },
+        bindto: "#Chart"
+    }) 
+});
+
+var MovePage = new Vue({
+    el: '#section',
+    methods: {
+        next: function () {
+            if (pageNum < maxPageNum) pageNum++
+            else alert("마지막 페이지입니다.")
+            var chart = bb.generate({
+                data: {
+                    columns: [starData[pageNum]]
+                },
+                axis: {
+                    y: {
+                      max: 10,
+                      min: 1
+                    }
+                },
+                color: {
+                    pattern: [
+                        "#ffe874"
+                    ]
+                },
+                bindto: "#Chart"
+            });
+        },
+        prev: function () {
+            if (pageNum > 0) pageNum--
+            else alert("처음 페이지입니다.")
+            var chart = bb.generate({
+                data: {
+                    columns: [starData[pageNum]]
+                },
+                axis: {
+                    y: {
+                      max: 10,
+                      min: 1
+                    }
+                  },
+                  color: {
+                    pattern: [
+                        "#ffe874"
+                    ]
+                },
+                bindto: "#Chart"
+            });
+        }
+    }
+})
